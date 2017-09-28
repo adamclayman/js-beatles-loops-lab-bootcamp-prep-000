85db3ee2566701742d95d8ea49cc667b0ab4d25b
Adam Clayman <adam@clayman.org>
2017-09-28 18:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i=0; i<musicians.length; i++) {
+    console.log(musicians[i] + ' plays ' + instruments[i]);
+  }
+}
